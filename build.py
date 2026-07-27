6efd1b8cddccf06b16af188f2358282b8badf9af
yunfan0211-crypto <yunfan0211@gmail.com>
2026-07-27 15:42:21 +0800

    build: fix portable packer exe filename claildesk-portable-packer
    After renaming the Rust binary to claildesk-portable-packer.exe,
    workflow and build scripts were still looking for rustdesk-portable-packer.exe.

--- build.py
+++ build.py
@@ -466,10 +466,10 @@ def build_flutter_windows(version, features, skip_portable_pack):
         f'python3 ./generate.py -f ../../{flutter_build_dir_2} -o . -e ../../{flutter_build_dir_2}/rustdesk.exe')
     os.chdir('../..')
     if os.path.exists('./rustdesk_portable.exe'):
-        os.replace('./target/release/rustdesk-portable-packer.exe',
+        os.replace('./target/release/claildesk-portable-packer.exe',
                    './rustdesk_portable.exe')
     else:
-        os.rename('./target/release/rustdesk-portable-packer.exe',
+        os.rename('./target/release/claildesk-portable-packer.exe',
                   './rustdesk_portable.exe')
     print(
         f'output location: {os.path.abspath(os.curdir)}/rustdesk_portable.exe')
